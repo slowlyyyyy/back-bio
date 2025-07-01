d1842658ade0f94a09de7d5342f82b2136e05217
unknown <nilso80@gmail.com>
2025-06-30 23:11:26 -0300

    'update'

--- index.js
+++ index.js
@@ -154,6 +154,6 @@ app.get('/questionario', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
